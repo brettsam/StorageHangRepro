8afba1ebddce05c500e2e833b9c9867db687fa5c
Brett Samblanet <brettsam@microsoft.com>
2019-08-02 15:54:59 -0700

    adding repro

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var account = CloudStorageAccount.Parse("");
+            var client = account.CreateCloudBlobClient();
+            var container = client.GetContainerReference("test");
+
+            ResponseListener.Initialize();
+
+            while (true)
+            {
+                Console.WriteLine(Environment.NewLine + "Listing -- " + DateTime.Now.ToLongTimeString());
+
+                BlobResultSegment segment = null;
+
+                string clientRequestId = Guid.NewGuid().ToString();
+
+                using (ResponseListener.TrackRequest(clientRequestId))
+                {
+                    CancellationTokenSource delayCts = new CancellationTokenSource();
+                    OperationContext context = new OperationContext { ClientRequestID = clientRequestId };
+
+                    Task delay = Task.Delay(TimeSpan.FromMinutes(1), delayCts.Token);
+                    Task<BlobResultSegment> segmentTask = container.ListBlobsSegmentedAsync(prefix: null, useFlatBlobListing: true,
+                       blobListingDetails: BlobListingDetails.None, maxResults: 5000, currentToken: null,
+                       options: null, operationContext: context);
+
+                    Task finishedTask = await Task.WhenAny(delay, segmentTask);
+
+                    if (Equals(finishedTask, delay))
+                    {
+                        Console.WriteLine("Hang detected. Disposing response to unblock.");
+                        ResponseListener.UnblockResponse(clientRequestId);
+
+                        try
+                        {
+                            // Depending on where it was blocked, this may actually retry and succeed.
+                            segment = await segmentTask;
+                        }
+                        catch (OperationCanceledException)
+                        {
+                        }
+                    }
+                    else
+                    {
+                        segment = await segmentTask;
+                    }
+
+                    delayCts.Cancel();
+                    delayCts.Dispose();
+                }
+
+                Console.WriteLine("Listed  -- " + DateTime.Now.ToLongTimeString() + " -- " + segment?.Results?.Count());
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ResponseListener.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+
+namespace ConsoleApp1
+{
+    public class ResponseListener : IObserver<DiagnosticListener>, IObserver<KeyValuePair<string, object>>
+    {
+        private static ConcurrentDictionary<string, HttpContent> _responseContents = new ConcurrentDictionary<string, HttpContent>();
+
+        private ResponseListener()
+        {
+        }
+
+        public static void Initialize()
+        {
+            DiagnosticListener.AllListeners.Subscribe(new ResponseListener());
+        }
+
+        public void OnCompleted()
+        {
+        }
+
+        public void OnError(Exception error)
+        {
+        }
+
+        public void OnNext(DiagnosticListener value)
+        {
+            if (value.Name == "HttpHandlerDiagnosticListener")
+            {
+                value.Subscribe(this);
+            }
+        }
+
+        public static IDisposable TrackRequest(string clientRequestId)
+        {
+            _responseContents[clientRequestId] = null;
+            return new ContentRemover(() => _responseContents.Remove(clientRequestId, out HttpContent value));
+        }
+
+        public static void UnblockResponse(string clientRequestId)
+        {
+            _responseContents[clientRequestId].Dispose();
+        }
+
+        public void OnNext(KeyValuePair<string, object> value)
+        {
+            if (value.Key == "System.Net.Http.HttpRequestOut.Stop")
+            {
+                var prop = value.Value.GetType().GetProperty("Response");
+                var response = prop.GetValue(value.Value) as HttpResponseMessage;
+
+                if (response.RequestMessage.Headers.TryGetValues("x-ms-client-request-id", out IEnumerable<string> headerValues))
+                {
+                    string clientRequestId = headerValues.Single();
+
+                    if (_responseContents.Keys.Contains(clientRequestId))
+                    {
+                        _responseContents[clientRequestId] = response.Content;
+                    }
+                }
+            }
+        }
+
+        private class ContentRemover : IDisposable
+        {
+            private Action _removalAction;
+
+            public ContentRemover(Action removalAction)
+            {
+                _removalAction = removalAction;
+            }
+
+            public void Dispose()
+            {
+                _removalAction();
+            }
+        }
+    }
+}
